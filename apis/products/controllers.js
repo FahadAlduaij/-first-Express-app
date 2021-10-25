c17ce628faaea0e384bf41397aa01635ea5494fd
fahadalduaij <fahad_osama@hotmail.com>
2021-10-25 18:57:45 +0300

    edit messge errors

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -6,8 +6,7 @@ exports.fetchProduct = (req, res) => {
 
 exports.createProduct = (req, res) => {
 	products.push(req.body);
-	res.status(201);
-	return res.json(req.body);
+	return res.status(201).json(req.body);
 };
 
 exports.deleteProduct = (req, res) => {
@@ -17,6 +16,6 @@ exports.deleteProduct = (req, res) => {
 		products = products.filter((product) => product.id !== +productId);
 		return res.status(204).end();
 	} else {
-		return res.status(404).json({ message: "Product not fount" });
+		return res.status(404).json({ message: "This Product not found" });
 	}
 };
