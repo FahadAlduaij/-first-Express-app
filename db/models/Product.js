75f11c0bbfc2100de9d631cc3b388493e752f921
fahadalduaij <fahad_osama@hotmail.com>
2021-10-26 19:46:39 +0300

    adding 	timestamps: true,

--- db/models/Product.js
+++ db/models/Product.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
+const productSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
 
-	price: {
-		type: Number,
-		default: 10,
-	},
-	quantity: {
-		type: Number,
-		min: 0,
-	},
+		price: {
+			type: Number,
+			default: 10,
+		},
+		quantity: {
+			type: Number,
+			min: 0,
+		},
 
-	date: { type: Date, default: Date.now },
-});
+		date: { type: Date, default: Date.now },
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
